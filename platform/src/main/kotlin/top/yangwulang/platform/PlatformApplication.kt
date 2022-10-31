ee1de6d84a6fc501feaf772d0c00bef765ef5e84
yangwulang <1021414627@qq.com>
2022-10-31 15:52:53 +0800

    Initial commit

---
+++ platform/src/main/kotlin/top/yangwulang/platform/PlatformApplication.kt
@@ -0,0 +1,11 @@
+package top.yangwulang.platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlatformApplication>(*args)
+}
---
+++ platform/src/test/kotlin/top/yangwulang/platform/PlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package top.yangwulang.platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

91ea51f9a62d873e20ebfed135cd5727b14915c4
yangwulang <1021414627@qq.com>
2022-11-28 15:23:41 +0800

    添加审计

--- free-platform-framework/src/main/kotlin/top/yangwulang/platform/config/JpaConfiguration.kt
+++ free-platform-framework/src/main/kotlin/top/yangwulang/platform/config/JpaConfiguration.kt
@@ -1,14 +1,18 @@
 package top.yangwulang.platform.config
 
+import org.apache.shiro.SecurityUtils
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.JpaVendorAdapter
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.Database
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import org.springframework.transaction.PlatformTransactionManager
+import java.util.*
 import javax.persistence.EntityManagerFactory
 import javax.sql.DataSource
 
@@ -18,6 +22,7 @@ import javax.sql.DataSource
 /*@EntityScan(value = [
     "top.yangwulang.platform.entity.book"
 ])*/
+@EnableJpaAuditing
 class JpaConfiguration {
 
 
@@ -42,8 +47,8 @@ class JpaConfiguration {
     @Bean
     fun entityManagerFactory(
         dataSource: DataSource,
-        jpaVendorAdapter : JpaVendorAdapter
-    ) : LocalContainerEntityManagerFactoryBean{
+        jpaVendorAdapter: JpaVendorAdapter
+    ): LocalContainerEntityManagerFactoryBean {
         val entityManagerFactoryBean = LocalContainerEntityManagerFactoryBean()
         entityManagerFactoryBean.dataSource = dataSource
         entityManagerFactoryBean.jpaVendorAdapter = jpaVendorAdapter
@@ -61,6 +66,25 @@ class JpaConfiguration {
         // 配置实体管理器工厂
         transactionManager.entityManagerFactory = entityManagerFactory
         return transactionManager
+    }
+
+    @Bean
+    fun auditor() : AuditorAware<String> {
+        // jpa审计bean，当有@CreateBy标注时会调用当前bean的getCurrentAuditor方法获得登录的用户信息
+        return UserAuditor()
+    }
+
+    inner class UserAuditor : AuditorAware<String> {
+        override fun getCurrentAuditor(): Optional<String> {
+            // 获取当前登录的用户信息
+            val primaryPrincipal = SecurityUtils.getSubject().principals.primaryPrincipal
+            return if (primaryPrincipal != null) {
+                Optional.ofNullable(primaryPrincipal as String)
+            } else {
+                Optional.empty()
+            }
+        }
+
     }
 
 }
--- free-platform-framework/src/main/kotlin/top/yangwulang/platform/entity/DataEntity.java
+++ free-platform-framework/src/main/kotlin/top/yangwulang/platform/entity/DataEntity.java
@@ -1,13 +1,20 @@
 package top.yangwulang.platform.entity;
 
 import io.swagger.annotations.ApiModelProperty;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class DataEntity<T extends DataEntity<T>> extends BaseEntity<T> {
 
     @Column(name = "status")
@@ -18,22 +25,26 @@ public class DataEntity<T extends DataEntity<T>> extends BaseEntity<T> {
     //@NotNull
     @Column(name = "create_by", nullable = false, length = 64)
     @ApiModelProperty("创建人")
+    @CreatedBy
     private String createBy;
 
     //@NotNull
     @Column(name = "create_date", nullable = false)
     @ApiModelProperty("创建时间")
+    @CreatedDate
     private Date createDate;
 
     @Size(max = 64)
     //@NotNull
     @Column(name = "update_by", nullable = false, length = 64)
     @ApiModelProperty("修改人")
+    @LastModifiedBy
     private String updateBy;
 
     //@NotNull
     @Column(name = "update_date", nullable = false)
     @ApiModelProperty("修改时间")
+    @LastModifiedDate
     private Date updateDate;
 
     @Size(max = 500)

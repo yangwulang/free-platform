09db74307a1cede227d26598dcc7c04a118c390f
yangwulang <1021414627@qq.com>
2022-12-22 08:50:16 +0800

    新增菜单时判断父节点是否存在

--- free-platform-framework/src/main/kotlin/top/yangwulang/platform/services/sys/impl/SysMenuServiceImpl.kt
+++ free-platform-framework/src/main/kotlin/top/yangwulang/platform/services/sys/impl/SysMenuServiceImpl.kt
@@ -1,9 +1,11 @@
 package top.yangwulang.platform.services.sys.impl
 
+import org.apache.commons.lang3.StringUtils
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import top.yangwulang.platform.entity.sys.SysMenu
+import top.yangwulang.platform.exception.ServiceException
 import top.yangwulang.platform.repository.sys.SysMenuRepository
 import top.yangwulang.platform.services.BaseServiceImpl
 import top.yangwulang.platform.services.sys.SysMenuService
@@ -30,6 +32,11 @@ open class SysMenuServiceImpl :
     @Transactional
     @Modifying
     override fun save(dto: SysMenu): SysMenu {
+        if (dto.parent != null && StringUtils.isNotEmpty(dto.parent.id)) {
+            dto.parent = repository.findById(dto.parent.id)
+                .orElseThrow {
+                    throw ServiceException("找不到pid为${dto.parent.id}的父菜单！") }
+        }
         return repository.save(dto)
     }
 }

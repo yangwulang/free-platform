10dce187436dd0e5e71ca0a025cc0756a7fcda78
yangwulang <1021414627@qq.com>
2023-02-14 21:22:42 +0800

    调整样式

--- free-platform-nuxt/layouts/default.vue
+++ free-platform-nuxt/layouts/default.vue
@@ -3,30 +3,36 @@
     <Layout class="layout-body">
       <Header class="layout-header" :style="{position: 'fixed', width: '100%', zIndex: 1}">
         <Menu mode="horizontal" :theme="theme" active-name="1">
-          <div class="layout-logo"></div>
+          <div class="layout-logo">
+
+          </div>
           <div class="layout-nav">
             <MenuItem name="1">
-              <Icon type="ios-navigate"></Icon>
-              Item 1
+              <Icon type="md-book" />
+              书籍
             </MenuItem>
             <MenuItem name="2">
-              <Icon type="ios-keypad"></Icon>
-              Item 2
+              <Icon type="logo-youtube" />
+              视频
             </MenuItem>
             <MenuItem name="3">
-              <Icon type="ios-analytics"></Icon>
-              Item 3
+              <Icon type="md-musical-note" />
+              音乐
             </MenuItem>
             <MenuItem name="4">
-              <Icon type="ios-paper"></Icon>
-              Item 4
+              <Icon type="md-chatbubbles" />
+              聊天
             </MenuItem>
           </div>
         </Menu>
       </Header>
-      <Content >
-        <slot name="body"></slot>
-      </Content>
+      <Layout>
+        <Sider class="layout-sider" hide-trigger>Sider</Sider>
+        <Content class="layout-content">
+          <slot name="body"></slot>
+        </Content>
+        <Sider class="layout-sider sider-right" style="width: 30px;min-width: 30px;max-width: 60px">Sider</Sider>
+      </Layout>
       <Footer class="layout-footer-center">2011-2016 &copy; View Design</Footer>
     </Layout>
   </div>
@@ -34,7 +40,7 @@
 </template>
 
 <script setup lang="ts">
-import {Icon, MenuItem, Menu, Content, Layout} from "view-ui-plus";
+import {Icon, MenuItem, Menu, Content, Layout, Sider} from "view-ui-plus";
 
 let theme = "light"
 </script>
@@ -43,9 +49,21 @@ let theme = "light"
 .layout {
   border: 1px solid #d7dde4;
   background: #000;
-  position: relative;
+  //position: relative;
   border-radius: 4px;
   overflow: hidden;
+  .layout-body {
+    .ivu-layout-content {
+      &:before {
+        content: ' ';
+        //position: absolute;
+        clear: both;
+        display: block;
+        height: 60px;
+        width: 100%;
+      }
+    }
+  }
 }
 
 .layout-logo {
@@ -66,5 +84,22 @@ let theme = "light"
 
 .layout-footer-center {
   text-align: center;
+}
+
+.layout-sider{
+  background: #ffffff;
+  border-left: 1px;
+  border-color: rgba(224,224,224,var(--tw-border-opacity));
+  &:before {
+    content: ' ';
+    //position: absolute;
+    clear: both;
+    display: block;
+    height: 60px;
+    width: 100%;
+  }
+}
+.sider-right {
+  background: rgba(255,255,255,var(--tw-bg-opacity));
 }
 </style>
--- free-platform-nuxt/pages/index/index.vue
+++ free-platform-nuxt/pages/index/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    index
     <NuxtWelcome/>
   </div>
 </template>

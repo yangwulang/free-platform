93d992ee44806189d97ef0c1aad41be934487490
yangwulang <1021414627@qq.com>
2023-11-03 20:16:34 +0800

    update: 添加注释

--- free-platform-framework-api/src/main/java/top/yangwulang/platform/entity/message/MessageRecordUser.java
+++ free-platform-framework-api/src/main/java/top/yangwulang/platform/entity/message/MessageRecordUser.java
@@ -9,16 +9,36 @@ import top.yangwulang.platform.entity.sys.User;
 
 import javax.validation.constraints.Null;
 
+/**
+ * 用户消息
+ *
+ * @author yangwulang
+ */
 @Entity
 @Table(name = "sys_message_record_user")
 public interface MessageRecordUser extends TypeBase {
 
+    /**
+     * 消息记录
+     *
+     * @return 消息
+     */
     @ManyToOne
     @Null MessageRecord record();
 
+    /**
+     * 用户
+     *
+     * @return 用户
+     */
     @ManyToOne
     @Null User user();
 
+    /**
+     * 是否已读
+     *
+     * @return 是否已读
+     */
     @Column(name = "is_read")
     Integer isRead();
 }
